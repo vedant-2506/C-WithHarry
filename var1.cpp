fc10d6a034229178b6d26289f38f50f826b62e41
vedant-2506 <swamidivate2506@gmail.com>
2025-09-04 22:43:46 +0530

    variables

---
+++ var1.cpp
@@ -0,0 +1,16 @@
+//using varaiablr
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=5;
+    int b=10;
+
+    float pi =3.14;
+
+    cout<<"Number "<<a<<" samller than "<<b;
+
+    cout<<"The value of pi is  "<<pi;
+}
